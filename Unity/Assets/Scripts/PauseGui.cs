978515b65428000c49c946cee14a013242e54e72
Basraga <sebastian.kasper96@gmx.at>
2014-01-30 11:08:29 +0100

    PauseGUI
    Weitere Optionen hinzugefügt.
    End-Button funktioniert jetzt

--- Unity/Assets/Scripts/PauseGui.cs
+++ Unity/Assets/Scripts/PauseGui.cs
@@ -6,6 +6,7 @@ public class PauseGui : MonoBehaviour {
 	public ControlControl cc;
 	// Use this for initialization
 	public bool showCursorChGUI = false;
+	public bool showOptionsGUI = false;
 
 	void Start () {
 	
@@ -21,15 +22,22 @@ public class PauseGui : MonoBehaviour {
 		GUI.Box(new Rect(Screen.width/2 - 100,Screen.height/2 - 50,200,210),"");	
 		if(GUI.Button(new Rect(Screen.width/2 - 50,Screen.height/2 -25,100,50),"Continue"))
 			Close();
-		if(GUI.Button (new Rect(Screen.width/2-50,Screen.height/2 +30,100,50),"Change Cursor"))
-			ChCursor();
+		if(GUI.Button (new Rect(Screen.width/2-50,Screen.height/2 +30,100,50),"Options"))
+			Options();
 		if(GUI.Button(new Rect(Screen.width/2 -50, Screen.height/2+85,100,50),"End"))
 			End ();
 
+		if(!showOptionsGUI)return;
+		GUI.Box (new Rect(Screen.width/2 + 110, Screen.height/2 + 5,200,155),"");
+		if(GUI.Button (new Rect (Screen.width/2 +160, Screen.height/2 + 30, 100,50),"Change Cursor"))
+			ChCursor ();
+		if(GUI.Button (new Rect (Screen.width /2 + 160, Screen.height /2 +85, 100,50),"Back"))
+			BackOptions();
+
 		if(!showCursorChGUI)return;
-		GUI.Box (new Rect(Screen.width/2+110,Screen.height/2 +5,200,200),"");
-		if(GUI.Button (new Rect(Screen.width/2 +160,Screen.height/2 +30,100,50),"Back"))
-			Back();
+		GUI.Box (new Rect(Screen.width/2+320,Screen.height/2 +60,200,100),"");
+		if(GUI.Button (new Rect(Screen.width/2 +370,Screen.height/2 +85,100,50),"Back"))
+			BackCursor();
 
 
 	}
@@ -37,21 +45,48 @@ public class PauseGui : MonoBehaviour {
 	void Close()
 	{
 		showCursorChGUI = false;
+		showOptionsGUI = false;
 		cc.toNormal();
 	}
-	void ChCursor()
+
+	void Options()
 	{
-		showCursorChGUI = true;
-	
+		if(showOptionsGUI == false)
+		{
+			showOptionsGUI = true;
+		}
+		else
+		{
+			showCursorChGUI = false;
+			showOptionsGUI = false;
+		}
 	}
 
-	void Back()
+	void ChCursor()
+	{
+		if (showCursorChGUI == false)
+		{
+			showCursorChGUI = true;
+		}
+		else
+			showCursorChGUI = false;
+	}
+
+	void BackOptions()
+	{
+
+		showCursorChGUI = false;
+		showOptionsGUI = false;
+	}
+
+	void BackCursor()
 	{
 		showCursorChGUI = false;
 	}
 
 	void End()
 	{
+		UnityEditor.EditorApplication.isPlaying = false;
 		Application.Quit();
 	}
 }
