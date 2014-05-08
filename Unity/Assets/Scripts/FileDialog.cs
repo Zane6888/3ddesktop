8c6f7414462471375aad327ec477601ab40d99d8
Basraga <sebastian.kasper96@gmx.at>
2014-05-08 11:10:42 +0200

    Added FileDialog.cs (no function)
    Because of Incompatibility of Unity3d and Windows.Forms

---
+++ Unity/Assets/Scripts/FileDialog.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FileDialog : MonoBehaviour {
+
+	//windows.Forms.OpenFileDialog funktioniert nicht, da Unity3d
+	//nicht damit kompatibel ist. 
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
