45448f5119e8ede3af90cd2a6b6af2e5116da2b6
Zane6888 <simon.wohlfart@gmx.at>
2014-06-05 10:32:06 +0200

    Fixed #27

--- Unity/Assets/Scripts/raycast.cs
+++ Unity/Assets/Scripts/raycast.cs
@@ -11,18 +11,12 @@ public class raycast : MonoBehaviour {
 	private GameObject currentMenuObject;
 	
 	public ControlControl cc;
-	//public GameObject room;
-	
-	void Start () {}
 	
 	// Update is called once per frame
-	void Update () {
-		
-
-		
+	void Update () 
+	{
 		if(Input.GetMouseButtonUp(0))
 		{
-			Debug.Log ("left hit");
 			RaycastHit hit;
 			Vector3 rayDirection = Cam.transform.forward;
 			Ray selectionRay = new Ray(Cam.transform.position, rayDirection);
@@ -34,18 +28,14 @@ public class raycast : MonoBehaviour {
 				{
 					Debug.Log(hit.collider.gameObject.name);
 					hit.collider.gameObject.GetComponent<ShortcutScript>().data.run();
-
 				}
 				if(hit.collider.tag == "menu_rename" && carriedObject == null)
 				{
 					cc.renameGui.show(currentMenuObject.GetComponentInChildren<ShortcutScript>().data);
-					IconGui.i.close();
 					cc.pause();
-
 				}
 				if(hit.collider.tag == "menu_move" && carriedObject == null)
 				{
-					IconGui.i.close();
 					carriedObject = currentMenuObject;
 					start.store.fillPlaceHolders("objectmove");
 					start.store.removeShortCut(carriedObject);
@@ -55,17 +45,16 @@ public class raycast : MonoBehaviour {
 				{ 
 					Vector3 targetLoc = start.store.findShortcut(hit.collider.gameObject);
 					start.store.removePlaceHolders("objectmove");
-					Debug.Log ("loc"+targetLoc);
 					if(targetLoc.x != -1)
 						start.store.addShortCut(carriedObject,(int)targetLoc.z,(int)targetLoc.x,(int)targetLoc.y);
 					carriedObject = null;
 				}
 			}
+			IconGui.i.close();
 		}
 		
 		if(Input.GetMouseButtonUp(1))
 		{
-			Debug.Log ("right hit");
 			RaycastHit hit;
 			Vector3 rayDirection = Cam.transform.forward;
 			Ray selectionRay = new Ray(Cam.transform.position, rayDirection);
@@ -77,10 +66,12 @@ public class raycast : MonoBehaviour {
 				if(hit.collider.tag == "shortcut")
 				{
 					currentMenuObject = hit.collider.gameObject;
-					
 					IconGui.i.showFor(player,currentMenuObject);
+					return;
 				}
 			}
+
+			IconGui.i.close();
 		}
 	}
 }
